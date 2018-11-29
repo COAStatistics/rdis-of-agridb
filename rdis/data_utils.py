68ce3a9ac75727ce369c31effcb0a14d52693e02
timy116 <timy117@gmail.com>
2018-11-29 19:08:50 +0800

    Update data_utils, generate_official_data
    data_utils:
    add simple_obj_creator fn
    is_exist fn
    __get_member fn
    get_household fn
    get_farmer_insurance fn
    get_elder_allowance fn
    generate...py:
    add household_data fn
    extract_data fn

--- rdis/data_utils.py
+++ rdis/data_utils.py
@@ -1,6 +1,65 @@
-def simple_obj_creator(text):
-    ...
+import shelve
+from collections import namedtuple
+
+SAMPLE_ATTR = ['layer', 'name', 'tel', 'addr', 'county', 'town',
+               'link_num', 'id', 'farmer_num', 'main_tpye', 'area', 'sample_num']
+
+PERSON_ATTR = ['addr_code', 'id', 'birthday', 'hh_num', 'addr',
+               'role', 'annotation', 'h_type', 'h_code']
+
+Sample = namedtuple('Sample', SAMPLE_ATTR)
+Person = namedtuple('Person', PERSON_ATTR)
+
+
+def simple_obj_creator(text, is_person=True):
+    l = text.strip().split('\t')
+    if is_person:
+        return Person._make(l)
+    else:
+        return Sample._make(l)
 
 
 class DataGenerator:
-    ...
+    with shelve.open('../agridb') as f:
+        __AMPL = f['appid_member_pk_link']
+        __M = f['member']
+        __HNMPL = f['household_num_members_pk_link']
+        __ROLE = f['role']
+        __CODE = {0: '', 1: '死亡', 2: '除戶'}
+        __MPFIPL = f['member_pk_farmer_insurance_pk_link']
+        __MPEPL = f['member_pk_elder_pk_link']
+
+    @classmethod
+    def is_exist(cls, appid) -> bool:
+        exist = cls.__AMPL.get(appid)
+        return True if exist else False
+
+    @classmethod
+    def __get_member(cls, pk):
+        return cls.__M.get(pk)
+
+    @classmethod
+    def get_household(cls, samp_id) -> list:
+        sample = cls.__get_member(cls.__AMPL[samp_id])
+        household = []
+        for i in cls.__HNMPL[sample['household']]:
+            member = cls.__get_member(i)
+            member['code'] = cls.__CODE[member['code']]
+            member['role'] = cls.__ROLE[member['role']]
+            household.append(member)
+        return household
+
+    @classmethod
+    def get_farmer_insurance(cls, appid):
+        if cls.__MPFIPL.get(appid):
+            return 'Y'
+        else:
+            return ''
+
+    @classmethod
+    def get_elder_allowance(cls, appid):
+        if cls.__MPEPL.get(appid):
+            return 'Y'
+        else:
+            return ''
+
--- rdis/generate_official_data.py
+++ rdis/generate_official_data.py
@@ -1,6 +1,11 @@
 import linecache as lc
-import data_utils
-from data_utils import DataGenerator
+from datetime import date
+from data_utils import (
+    DataGenerator as dg,
+    simple_obj_creator,
+)
+
+YEAR = date.today().year
 
 
 def load_sample(num) -> None:
@@ -9,14 +14,44 @@ def load_sample(num) -> None:
     :param num: 3:main, 4:main_inv, 5:sub, 6:sub_inv
     :return: None
     """
-
     path = 'info.txt'
     file = lc.getline(path, num).strip()
     with open(file, encoding='utf8') as f:
-        for line in enumerate(f):
-            ...
+        for i, line in enumerate(f):
+            obj = simple_obj_creator(line, is_person=False)
+            exist = dg.is_exist(obj.id)
+            if exist:
+                household_data(obj)
+            else:
+                print(obj, exist)
+            if i == 10000:
+                break
+
+
+def household_data(samp, exist=True):
+    data = []
+    if exist:
+        household = dg.get_household(samp.id)
+        for member in household:
+            extract_data(member)
+    else:
+        extract_data(samp, exist)
+
+
+def extract_data(member, exist=True):
+    person_data = [''] * 11
+    if exist:
+        person_data[0] = str(YEAR - int(member['birth'][:4]))
+        person_data[1] = member['role']
+        person_data[2] = member['code']
+        person_data[3] = dg.get_farmer_insurance(member['id'])
+        person_data[4] = dg.get_elder_allowance(member['id'])
+        print(person_data)
+    else:
+        ...
 
 
 if __name__ == '__main__':
+    dg = dg()
     sample_num = eval(input('sample_num = '))
     load_sample(sample_num)
